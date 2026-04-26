b8bb5c874604605a0d27725fdc61f45318705ead
Синенко Роман Васильевич <roman.sinenko@vseinstrumenti.ru>
2026-04-26 22:44:29 +0300

    Initial Commit

---
+++ ChatIOS/ChatIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatIOSApp.swift
+//  ChatIOS
+//
+//  Created by Roman Sinenko on 26.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChatIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
